b2c109849fec0c4fe7a6026d54b648cba8762f07
Acip <freshbone.acip@yahoo.co.id>
2025-10-29 21:45:57 +0800

    add lorem ipsum generator

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -88,5 +88,17 @@ return {
       --   width = "block",
       -- }
     },
+  },
+  {
+    -- Lorem Ipsum Generator
+    'derektata/lorem.nvim',
+    config = function()
+      require("lorem").opts {
+        sentence_length = "mixed", -- using a default configuration
+        comma_chance = 0.3, -- 30% chance to insert a comma
+        max_commas = 2, -- maximum 2 commas per sentence
+        debounce_ms = 300, -- default debounce time in milliseconds
+      }
+    end
   }
 }
